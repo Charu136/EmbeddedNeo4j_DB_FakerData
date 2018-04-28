28db7f81e1d4e8f667889b6372405cf92ffbf704
Aneri <aneripatel12351@gmail.com>
2018-04-27 23:31:45 -0700

    Update PopulateData.java

--- src/main/java/Proj2/Project2/PopulateData.java
+++ src/main/java/Proj2/Project2/PopulateData.java
@@ -28,37 +28,36 @@ public class PopulateData {
 
 	GraphDatabaseService graphDb;
 
+	// Types of Labels
 	public static enum LabelTypes implements Label {
 		USER, LOCATION, COMPANY, JOINING_DATE
 
 	}
 
-	// START SNIPPET: createReltype
+	// Types of Relationships
 	public static enum RelTypes implements RelationshipType {
 		HAS_EMPLOYED, CREATED_ACCOUNT_ON, LIVES_AT
 	}
 
-	// END SNIPPET: createReltype
 	ArrayList<Node> user_list = new ArrayList<>();
 	ArrayList<Node> company_list = new ArrayList<>();
 	ArrayList<Node> location_list = new ArrayList<>();
 	ArrayList<Node> joining_date_list = new ArrayList<>();
 
+	//method to populate data using Faker and neo4j
 	void insertData(GraphDatabaseService graphDb1) {
 		graphDb = graphDb1;
 		graphDb.beginTx();
 		Faker faker = new Faker();
 
-		int total_names = 1000;
-		int total_addresses = 1000;
-		int total_companies = 1000;
-		int total_dates = 1000;
+		int total_names = 10000;
+		int total_addresses = 10000;
+		int total_companies = 10000;
+		int total_dates = 10000;
 
 		for (int i = 0; i < total_names; i++) {
 
-			// Database operations go here
-			// END SNIPPET: transaction
-			// START SNIPPET: addData
+			// nodes created with Label type USER and properties FIRSTNAME and LASTNAME
 			Node newNode = graphDb.createNode();
 			newNode.addLabel(LabelTypes.USER);
 			newNode.setProperty("firstName", faker.name().firstName());
@@ -68,23 +67,17 @@ public class PopulateData {
 
 		for (int i = 0; i < total_companies; i++) {
 
-			// Database operations go here
-			// END SNIPPET: transaction
-			// START SNIPPET: addData
+			// // nodes created with Label type COMPANY and properties COMPANY NAME, LOGO and INDUSTRY
 			Node newNode = graphDb.createNode();
 			newNode.addLabel(LabelTypes.COMPANY);
 			newNode.setProperty("companyName", faker.company().name());
 			newNode.setProperty("logo", faker.company().logo());
 			newNode.setProperty("industry", faker.company().industry());
-			// newNode.setProperty("profession", faker.company().profession());
-
 		}
 
 		for (int i = 0; i < total_addresses; i++) {
 
-			// Database operations go here
-			// END SNIPPET: transaction
-			// START SNIPPET: addData
+			// // nodes created with Label type LOCATION and properties CITY, STATE and STREET ADDRESS
 			Node newNode = graphDb.createNode();
 			newNode.addLabel(LabelTypes.LOCATION);
 			newNode.setProperty("city", faker.address().city());
@@ -96,15 +89,14 @@ public class PopulateData {
 
 		for (int i = 0; i < total_dates; i++) {
 
-			// Database operations go here
-			// END SNIPPET: transaction
-			// START SNIPPET: addData
+			// // nodes created with Label type JOINING DATE and properties PAST
 			Node newNode = graphDb.createNode();
 			newNode.addLabel(LabelTypes.JOINING_DATE);
 			newNode.setProperty("past", faker.date().past(3650, TimeUnit.DAYS).toString());
 
 		}
 
+		// iterate over nodes to establish relationships among them
 		try (ResourceIterator<Node> users = graphDb.findNodes(LabelTypes.USER)) {
 			ResourceIterator<Node> companies = graphDb.findNodes(LabelTypes.COMPANY);
 
@@ -117,13 +109,7 @@ public class PopulateData {
 			while (companies.hasNext()) {
 				company_list.add(companies.next());
 			}
-			/*
-			 * int k = 0; for (int i = 0; i < company_list.size(); i++) { for (int j = k; i
-			 * <20; j++) { company_list.get(i).createRelationshipTo(user_list.get(j),
-			 * RelTypes.HAS_EMPLOYED); } k=k+20;
-			 * 
-			 * }
-			 */
+			
 			while (locations.hasNext()) {
 				location_list.add(locations.next());
 			}
@@ -148,17 +134,7 @@ public class PopulateData {
 
 	}
 	
-	
-	String toString(Node n, String prop) {
-		return "("+n.getProperty(prop)+")";
-		}
-	
-	
-	String toString(Relationship r) {
-		return "-["+r.getType().name()+"]->"; 
-		}
-	
-
+	//query to get list of users living at certain locations
 	public void getUsersLocations(final Node person) {
 		graphDb.beginTx();
 		String output = "";
@@ -166,26 +142,12 @@ public class PopulateData {
 				.relationships(RelTypes.LIVES_AT, Direction.OUTGOING).evaluator(Evaluators.excludeStartPosition());
 
 		for (Path userPath : td.traverse(person)) {
-			output=render(userPath, UserProperties.FIRSTNAME);
+			System.out.println(userPath);
 		}
-		
-	
-		
-		
 		System.out.println("----------------------------------------------------------------------------------------------------------------------------");
 	}
 
-	public String render(Path path, String prop) {
-		StringBuilder sb = new StringBuilder();
-		for (PropertyContainer pc : path) {
-			if (pc instanceof Node)
-				sb.append(toString((Node) pc, prop));
-			else
-				sb.append(toString((Relationship) pc));
-		}
-		return sb.toString();
-	}
-
+	//query to get users who work at different companies
 	public void getCompanyUsers(final Node person) {
 		graphDb.beginTx();
 		String output = "";
@@ -195,10 +157,10 @@ public class PopulateData {
 		for (Path companyPath : td.traverse(person)) {
 			System.out.println(companyPath);
 		}
-		System.out.println(
-				"----------------------------------------------------------------------------------------------------------------------------");
+		System.out.println("----------------------------------------------------------------------------------------------------------------------------");
 	}
 
+	//query to fetch the users who have joined in the near past of 10 years
 	public void getJoiningDatesOfUsers(final Node person) {
 		graphDb.beginTx();
 		String output = "";
@@ -211,19 +173,5 @@ public class PopulateData {
 		}
 
 	}
-
-	/*
-	 * public void findEmployeeList( final Node startNode ) {
-	 * System.out.println(""); graphDb.beginTx(); TraversalDescription td =
-	 * graphDb.traversalDescription() .breadthFirst() .relationships(
-	 * RelTypes.HAS_EMPLOYED, Direction.OUTGOING ) .relationships(
-	 * RelTypes.LIVES_AT, Direction.OUTGOING ) .evaluator(
-	 * Evaluators.includeWhereLastRelationshipTypeIs( RelTypes.LIVES_AT ) ); String
-	 * output=""; int numberOfHackers=0; for ( Path friendPath :
-	 * td.traverse(startNode) ) { output += "At depth " + friendPath.length() +
-	 * " => " + friendPath.endNode() .getProperty( "name" ) + "\n";
-	 * numberOfHackers++; } output += "Number of hackers found: " + numberOfHackers
-	 * + "\n"; }
-	 */
 
 }
