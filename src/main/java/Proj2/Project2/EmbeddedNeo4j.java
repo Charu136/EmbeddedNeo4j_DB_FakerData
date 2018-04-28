349d31943b050303a5146bcc847855385b0fd8d9
Aneri <aneripatel12351@gmail.com>
2018-04-27 23:36:28 -0700

    Update EmbeddedNeo4j.java

--- src/main/java/Proj2/Project2/EmbeddedNeo4j.java
+++ src/main/java/Proj2/Project2/EmbeddedNeo4j.java
@@ -31,19 +31,27 @@ public class EmbeddedNeo4j {
 
 	}
 
+	//creates neo4j database 
 	void createDb() throws IOException {
 
 		graphDb = new GraphDatabaseFactory().newEmbeddedDatabase(databaseDirectory);
 		PopulateData pd = new PopulateData();
+		//populates database by calling this method
 		pd.insertData(graphDb);
+		
+		//iterates over database and execute queries
 		ResourceIterator<Node> users = graphDb.findNodes(PopulateData.LabelTypes.USER);
 		ResourceIterator<Node> companies = graphDb.findNodes(PopulateData.LabelTypes.COMPANY);
+		ResourceIterator<Node> join_dates = graphDb.findNodes(PopulateData.LabelTypes.JOINING_DATE);
 		while (users.hasNext()) {
 			pd.getUsersLocations(users.next());
 			
 		}
 		while(companies.hasNext()) { 
 			 pd.getCompanyUsers(companies.next());
+		 }
+		while(join_dates.hasNext()) { 
+			 pd.getJoiningDatesOfUser(join_dates.next());
 		 }
 		
 	}
